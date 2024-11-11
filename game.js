152531233d7fa8452ae065109150787750768fb0
TazerVN <157727133+TazerVN@users.noreply.github.com>
2024-11-11 21:45:15 +0700

    feat: remove text box and added UI to the game

--- game.js
+++ game.js
@@ -1,29 +1,94 @@
 let humanScore = 0;
 let computerScore = 0;
-playGame(5)
+let round = 0
+let userActiveInput = false;
 
-function playGame(round){
-    for (let i = 0; i < round; i++){
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice()
-        if (humanSelection == "nothing"){break}
-        playRound(humanSelection,computerSelection)
+const options = document.createElement("div")
+options.classList.add("options")
+const input = document.querySelector(".input")
+const scoreboard = document.querySelector(".score")
+const currentscore = document.createElement("div")
+const text = document.createElement("h2")
+const computertext = document.querySelector(".computer")
+
+
+const rock = document.createElement("button")
+rock.textContent = "Rock"
+
+
+const paper = document.createElement("button")
+paper.textContent = "Paper"
+
+
+const scissors = document.createElement("button")
+scissors.textContent = "Scissors"
+
+input.appendChild(options)
+scoreboard.appendChild(text)
+scoreboard.appendChild(currentscore)
+
+
+options.appendChild(rock)
+options.appendChild(paper)
+options.appendChild(scissors)
+
+rock.addEventListener("click", () => {
+    const humanChoice = getHumanChoice("Rock")
+    Gameplay(humanChoice)
+})
+paper.addEventListener("click", () => {
+    const humanChoice = getHumanChoice("Paper")
+    Gameplay(humanChoice)
+})
+scissors.addEventListener("click", () => {
+    const humanChoice = getHumanChoice("Scissors")
+    Gameplay(humanChoice)
+})
+
+UpdateScoreBoard(humanScore,computerScore,"pick your move")
+
+function Gameplay(humanChoice){
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice)
+    round += 1;
+    endGame();
+}
+
+function endGame(){
+    if (humanScore === 5 || computerScore === 5){
+    switch (true){
+        case (humanScore > computerScore):
+            UpdateScoreBoard(humanScore, computerScore,"Congrats! You beat the computer, " + humanScore + " to " + computerScore + " !")
+            alert("Congrats! You beat the computer")
+            break
+        case (humanScore < computerScore):
+            UpdateScoreBoard(humanScore, computerScore,"Aww man! The Computer beat you, " + computerScore + " to " + humanScore + " !");
+            alert("Aww man! The Computer beat you")
+            break
+        case (humanScore == computerScore):
+            UpdateScoreBoard(humanScore, computerScore,"It's a tie, " + humanScore + " to " + computerScore + " !")
+            alert("It's a tie!")
+            break
     }
-    if (humanScore < computerScore){console.log("Computer Win! Suck it noob!")}
-    else if (humanScore > computerScore){console.log("Human Win! AI will come after your job!")}
-    else {console.log("It's a tie!")}
+    }
+}
+function computerMove(text){
+    computertext.textContent = text
 }
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3)
    if (choice == 0){
-    return "rock"
+    computerMove("Computer choose Rock")
+    return "Rock"
    }
    else if (choice == 1){
-    return "scissors"
+    computerMove("Computer choose Scissors")
+    return "Scissors"
    }
    else {
-    return "paper"
+    computerMove("Computer choose Paper")
+    return "Paper"
    }
 }
 
@@ -33,65 +98,49 @@ function generate(){
     }
 }
 
-function promptShow(){
-    let text = prompt("Rock, Paper or Scissors?")
-    if (text == null | text == undefined)
-        {return "nothing"}
-    else {
-    return text.toLowerCase()}
+function UpdateScoreBoard(humanScore,computerScore,extratext){
+    text.textContent = extratext
+    currentscore.textContent = "Human Score: " + humanScore + " | Computer Score: " + computerScore
 }
 
-function getHumanChoice(){
-    let text = promptShow()
-    if (text=="rock" || text.toLowerCase()=="paper" || text.toLowerCase()=="scissors"){
-        return text
-    }
-    else {
-        console.log("Invalid Choice")
-        return
-    }
- }
+
+function getHumanChoice(choice){
+    console.log("Human choose " + choice)
+    return choice    
+}
 
 
  function playRound(humanChoise, computerChoice){
     if(humanChoise == "nothing"){return "nothing"}
 
-    else if (humanChoise == "rock" && computerChoice == "scissors"){
-        console.log("Human wins! Rock beats Scissors")
+    else if (humanChoise == "Rock" && computerChoice == "Scissors"){
         humanScore++
-        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore)
+        UpdateScoreBoard(humanScore,computerScore,"Human wins! Rock beats Scissors")
     }
-    else if (humanChoise == "rock" && computerChoice == "paper"){
-        console.log("Computer wins! Paper beats Rock")
+    else if (humanChoise == "Rock" && computerChoice == "Paper"){
         computerScore++
-        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore)
+        UpdateScoreBoard(humanScore,computerScore, "Computer wins! Paper beats Rock")
     }
-    else if (humanChoise == "scissors" && computerChoice == "paper"){
-        console.log("Human wins! Scissors beats Rock")
+    else if (humanChoise == "Scissors" && computerChoice == "Paper"){
         humanScore++
-        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore)
+        UpdateScoreBoard(humanScore,computerScore, "Human wins! Scissors beats Rock")
     }
-    else if (humanChoise == "scissors" && computerChoice == "rock"){
-        console.log("Computer wins! Rock beats Scissors")
+    else if (humanChoise == "Scissors" && computerChoice == "Rock"){
         computerScore++
-        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore)
+        UpdateScoreBoard(humanScore,computerScore, "Computer wins! Rock beats Scissors")
     }
-    else if (humanChoise == "paper" && computerChoice == "rock"){
-        console.log("Human wins! Paper beats Rock")
+    else if (humanChoise == "Paper" && computerChoice == "Rock"){
         humanScore++
-        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore)
+        UpdateScoreBoard(humanScore,computerScore, "Human wins! Paper beats Rock")
     }
-    else if (humanChoise == "paper" && computerChoice == "scissors"){
-        console.log("Computer wins! Scissors beats Paper")
+    else if (humanChoise == "Paper" && computerChoice == "Scissors"){
         computerScore++
-        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore)
+        UpdateScoreBoard(humanScore,computerScore, "Computer wins! Scissors beats Paper")
     }
-    else if (humanChoise == computerChoice){
-        console.log("Draw! Both choices are the same")
-        console.log("Human Score: " + humanScore + " | Computer Score: " + computerScore)
+    else if (humanChoise === computerChoice){
+        UpdateScoreBoard(humanScore,computerScore, "Draw! Both choices are the same")
     }
-    else {
-        return "nothing"
+    else{
+        console.log("something wrong")
     }
- }
-
+}
